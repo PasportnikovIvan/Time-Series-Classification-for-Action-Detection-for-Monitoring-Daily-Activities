626e164eda6aa215980e59c16c6a03b5dabc5579
Ivan Pasportnikov <i.pasportnikov@mail.ru>
2025-05-05 00:22:40 +0200

    aplitting dataset to train, validation and test files

--- modules/split_dataset.py
+++ modules/split_dataset.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 
 def get_file_list(directory, action):
     action_dir = os.path.join(directory, action)
-    return [os.path.join(action_dir, file) for file in os.listdir(action_dir) if file.endswith("_repaired.json")]
+    return [os.path.join(action_dir, file) for file in os.listdir(action_dir) if file.endswith('.json')]
 
 def split_dataset(directory, actions, train_ratio=0.7, val_ratio=0.15, test_ratio=0.15):
     train_files = []
@@ -33,8 +33,8 @@ def save_split_files(train_files, val_files, test_files, output_dir="splits"):
         f.write("\n".join(test_files))
 
 actions = ['sppb', 'timed-up-and-go', 'falling', 'sitting', 'standing']
-camera_train, camera_val, camera_test = split_dataset("../dataset/cameraLandmarks", actions)
-global_train, global_val, global_test = split_dataset("../dataset/globalLandmarks", actions)
+camera_train, camera_val, camera_test = split_dataset("dataset/cameraLandmarks", actions)
+global_train, global_val, global_test = split_dataset("dataset/globalLandmarks", actions)
 
 print("Camera Train files:", len(camera_train))
 print("Camera Validation files:", len(camera_val))
